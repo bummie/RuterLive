eb776f6e48b6d9cbd8190951cf0414648e3ac008
Seebzei <seebzei@gmail.com>
2017-08-28 11:29:52 +0200

    La til stop og route

--- js/routemanager.js
+++ js/routemanager.js
@@ -1,1 +1,20 @@
 // Todo smart shit
+
+
+
+
+
+
+
+function Route()
+{
+    this.id = 0;
+    this.stops = new Array(0);
+}
+
+function Stop(id, name, position)
+{
+    this.id = id;
+    this.name = name;
+    this.position = position;
+}
